d35623ccf1886c0f2d21bc91d1e8d9ded26f4a4a
CBHANDA <BHANDAEHMA@GMAIL.COM>
2026-05-06 00:25:21 -0700

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,45 @@
+# ###### how to read strings
+
+str1='python'
+'''print(str1[0])
+print(str1[5])
+print(str1[-1])
+for i in str1:			#using a for loop
+    print(i)'''
+# using indexiing
+'''print(str1[0])
+a=str1[0:6:2]# start , stop , step {slicing}
+print(a)
+print(str1[-1:-3:-1])
+print(str1[2:6:1])
+'''
+#*******************************####
+
+# write a program, tto find  input string is pa
+
+'''
+str1=input("enter any string")
+str2=str1[::-1]
+if str1==str2:
+    print(f'{str1} is pal')
+else:
+    print(f'{str1} is not pal')
+'''
+
+#######*******************************########
+
+# *****METHOD OF STRINGS*****#
+
+#method 1
+'''str1="emma"
+print(str1.capitalize())
+'''
+#method2
+'''str1="emma"
+str2=str1.find('r')
+print(str2)'''
+#method 3
+'''st1="emma"
+st2=st1.index('r')
+print(st2)
+'''SSz
